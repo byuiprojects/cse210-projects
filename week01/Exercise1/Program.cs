47f54ca75bd0daeca175f47f838df12ef5c6d86d
byuiprojects <simonmaganzo1989@gmail.com>
2025-05-10 23:42:16 +0200

    completed first assignment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World.");
-        Console.WriteLine("this is my first c# program.");
+        Console.WriteLine("what is your  first name?");
+        string FirstName = Console.ReadLine();
+        Console.WriteLine("what is your last name?");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName},{FirstName} {lastName} ");
+    
 }
 }
